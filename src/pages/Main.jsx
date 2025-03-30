22add6e2c6e38c67e89a28c5f20be1667cd3bf83
huha123 <huha3213@gmail.com>
2025-03-30 16:42:59 +0900

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,6 +13,7 @@ const Main = () => {
       <div>base url : {import.meta.env.BASE_URL}</div>
       <div>TEST_SECRET_KEY : {import.meta.env.TEST_SECRET_KEY}</div>
       <div>TEST_PUBLIC_KEY : {import.meta.env.TEST_PUBLIC_KEY}</div>
+      <div>VITE_SOME_KEY_1 : {import.meta.env.VITE_SOME_KEY_1}</div>
     </div>
   );
 };
