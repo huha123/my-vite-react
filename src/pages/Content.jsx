5e9891622e3647a155a395d431a42ed5bfdfdabf
huha123 <huha3213@gmail.com>
2025-03-31 18:16:53 +0900

    1

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,19 +1,84 @@
+import { initializeApp } from "firebase/app";
+import {
+  child,
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  update,
+  remove,
+} from "firebase/database";
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Content = () => {
   const [searchParams] = useSearchParams();
+
+  const config = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  };
+  const app = initializeApp(config);
+  const db = getDatabase();
+  const id = crypto.randomUUID();
+  // const list = ref(db, "/date");
+
   useEffect(() => {
     console.log("### Content mounted !!!");
-    console.log("### searchParams:", searchParams);
-    searchParams.forEach((v) => {
-      console.log("@@@ foreach v:", v);
-    });
+    console.log(id);
     return () => {
       console.log("### clear Content unmount !!");
     };
   }, []);
-  return <div className="container">Content page</div>;
+
+  const getData = () => {
+    const list = ref(db, "/title");
+    onValue(ref(db, "/title"), (snapshot) => {
+      console.log("@@@ snapshot:", snapshot.val());
+    });
+  };
+
+  const setData = () => {
+    const uid = crypto.randomUUID();
+    const testData = {
+      uid: uid,
+      user: "user 1",
+      title: "title 1",
+      content: "content 1",
+    };
+    console.log("## testData:", testData);
+
+    const newPostKey = push(child(ref(db), "posts")).key;
+    console.log("## newPostKey:", newPostKey);
+
+    const updates = {};
+    updates["/posts/" + newPostKey] = testData;
+    updates["/user-posts/" + uid + "/" + newPostKey] = testData;
+    update(ref(db), updates);
+  };
+
+  const removeData = () => {
+    const updates = {};
+    updates["/posts/"] = null;
+    updates["/user-posts/"] = null;
+    update(ref(db), updates);
+    // remove(ref(db, "/posts"));
+  };
+
+  return (
+    <>
+      <div className="container">
+        <button onClick={getData}>get list</button>
+        <button onClick={setData}>insert test</button>
+        <button onClick={removeData}>remove test</button>
+      </div>
+      {/* <div className="container">{list}</div> */}
+    </>
+  );
 };
 
 export default Content;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,10 +10,6 @@ const Main = () => {
   return (
     <div className="container">
       <div>Main page11</div>
-      <div>VITE_SECRET_KEY : {import.meta.env.VITE_SECRET_KEY}</div>
-      <div>VITE_PUBLIC_KEY : {import.meta.env.VITE_PUBLIC_KEY}</div>
-      <div>VITE_LOCAL_VAL_1 : {import.meta.env.VITE_LOCAL_VAL_1}</div>
-      <div>REACT_APP_LOCAL_VAL_1 : {import.meta.env.REACT_APP_LOCAL_VAL_1}</div>
     </div>
   );
 };
