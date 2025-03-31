9c1a2215b6b6f8727051374b472f409ef626acf3
huha123 <huha3213@gmail.com>
2025-03-31 18:27:13 +0900

    1

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -17,7 +17,8 @@ const Content = () => {
   const config = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    // databaseURL: import.meta.env.VITE_DATABASE_URL,
+    databaseURL: "https://superbuild-dic.firebaseio.com",
     projectId: import.meta.env.VITE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
