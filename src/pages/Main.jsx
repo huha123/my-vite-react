4eeadc34a76bd98266e01ac28faa13e35ddc3737
huha123 <huha3213@gmail.com>
2025-03-31 14:25:38 +0900

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -12,6 +12,8 @@ const Main = () => {
       <div>Main page11</div>
       <div>VITE_SECRET_KEY : {import.meta.env.VITE_SECRET_KEY}</div>
       <div>VITE_PUBLIC_KEY : {import.meta.env.VITE_PUBLIC_KEY}</div>
+      <div>VITE_LOCAL_VAL_1 : {import.meta.env.VITE_LOCAL_VAL_1}</div>
+      <div>REACT_APP_LOCAL_VAL_1 : {import.meta.env.REACT_APP_LOCAL_VAL_1}</div>
     </div>
   );
 };
